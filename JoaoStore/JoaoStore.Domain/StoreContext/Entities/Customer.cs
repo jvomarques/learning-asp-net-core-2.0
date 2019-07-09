27458fb962cdbb8de0499fcec9e0836cdcc0e18c
João Victor Marques de Oliveira <09001597408@tce.govrn>
2019-07-09 10:42:43 -0300

    adding collections

--- JoaoStore/JoaoStore.Domain/StoreContext/Entities/Customer.cs
+++ JoaoStore/JoaoStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,24 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using JoaoStore.Domain.StoreContext.ValueOjects;
 
 namespace JoaoStore.Domain.StoreContext{
     public class Customer {
+
+        private readonly IList<Address> _addresses;
         public Customer(Name name, Document document, Email email, string phone)
         {
             this.name = name;
             this.document = document;
             this.email = email;
             this.phone = phone;
-            this.addresses = new List<Address>();;
+            this._addresses = new List<Address>();
         }
 
         public Name name { get; private set; }
         public Document document { get; private set; } 
         public Email email { get; private set; }
         public string phone { get; private set; }
-        public IReadOnlyCollection<Address> addresses { get; private set; }
+        public IReadOnlyCollection<Address> addresses => this._addresses.ToArray();
 
+        public void addAddress(Address address){
+            this._addresses.Add(address);
+        }
         public override string ToString(){
             return this.name.ToString();
         }
--- JoaoStore/JoaoStore.Domain/StoreContext/Entities/Order.cs
+++ JoaoStore/JoaoStore.Domain/StoreContext/Entities/Order.cs
@@ -1,28 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using JoaoStore.Domain.StoreContext.Enums;
 
 namespace JoaoStore.Domain.StoreContext{
     public class Order {
+
+        private readonly IList<OrderItem> _items;
+        private readonly IList<Delivery> _deliveries;
+
         public Order(Customer customer)
         {
             this.customer = customer;
             this.number = Guid.NewGuid().ToString().Replace("-", "").Substring(0,8).ToUpper();
             this.createDate = DateTime.Now;
             this.status = EOrderStatus.created;
-            this.items = new List<OrderItem>();
-            this.deliveries = new List<Delivery>();
+            this._items = new List<OrderItem>();
+            this._deliveries = new List<Delivery>();
         }
 
         public Customer customer { get; private set; }
         public string number { get; private set; }
         public DateTime createDate { get; private set; }
         public EOrderStatus status { get; private set; }
-        public IReadOnlyCollection<OrderItem> items { get; private set; }
-        public IReadOnlyCollection<Delivery> deliveries { get; private set; }
+        public IReadOnlyCollection<OrderItem> items => _items.ToArray();
+        public IReadOnlyCollection<Delivery> deliveries => _deliveries.ToArray();
 
         public void addItem(OrderItem item){
+            this._items.Add(item);
+        }
 
+        public void addDelivery(Delivery delivery){
+            this._deliveries.Add(delivery);
         }
         //Do order
         public void placeOrder(){
